505d4d1aca1d73edf943c11955a7bd961585bdfb
Galang Alfianto <galang.vendor.pasti@pegadaian.co.id>
2024-01-21 05:17:52 +0700

    add groovy checkout tahap 1

--- Include/features/Beli Jam Tangan.feature
+++ Include/features/Beli Jam Tangan.feature
@@ -1,9 +1,9 @@
 @beliJamTangan
 Feature: Pembelian Jam Tangan
 
-  @loginWithGoogle
+  @checkoutJamTangan
   Scenario: Pembelian Jam Tangan Sampai Check Out
     Given user akses halaman Jamtangan.com
-    When user pilih Jam Tangan
+    When user pilih Jam Tangan "casio"
     And user pilih metode pembayaran
     Then user berhasil melakukan pembayaran
--- Include/scripts/groovy/CheckOutJamTangan.groovy
+++ Include/scripts/groovy/CheckOutJamTangan.groovy
@@ -42,28 +42,52 @@ import cucumber.api.java.en.When
 
 
 class CheckOutJamTangan {
-	
+
 	@Given("user akses halaman Jamtangan.com")
-	def userAksesHompage() {
+	def userAksesHomepage() {
 		WebUI.openBrowser(null)
+
 		WebUI.navigateToUrl("https://www.jamtangan.com/")
+
 		WebUI.click(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//button[normalize-space()='Nanti Saja']"))
 	}
 
-	@When("user pilih Jam Tangan")
-	def userPilihJamTangan() {
-		WebUI.setText(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//input[@placeholder='Cari di Jamtangan.com']"), "casio")
-		WebUI.click(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//div[@class='flex items-center gap-4']"))
+	@When("user pilih Jam Tangan {string}")
+	def userPilihJamTangan(String merek) {
+		WebUI.setText(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//input[@placeholder='Cari di Jamtangan.com']"), merek)
+
+		WebUI.click(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//button[@aria-label='Search']"))
+
+		WebUI.waitForElementPresent(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//button[normalize-space()='Lewati']"), 3)
+
+		WebUI.click(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//button[normalize-space()='Lewati']"))
+
+		WebUI.click(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//div[normalize-space()='Rp2.051.000']"))
+
+		WebUI.verifyElementPresent(new TestObject().addProperty('id', ConditionType.EQUALS, 'btn-buy-now'), 0)
+
+		WebUI.click(new TestObject().addProperty('id', ConditionType.EQUALS, 'btn-buy-now'))
+		
+		WebUI.setText(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//input[@name='username']"), 'dospemkedua@gmail.com')
+		
+		WebUI.waitForElementPresent(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//input[@name='password']"), 3)
+		
+		WebUI.setText(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//input[@name='password']"), 'Fabrue927!')
+		
+		WebUI.click(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//button[@class='mw-ripple-effect btn rounded text-sm relative overflow-hidden w-full btn-filled text-neutral-1000 bg-primary-1 uppercase qa-login-button']"))
+
+		WebUI.verifyElementPresent(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//div[@class='grid grid-cols-4 text-neutral-300 text-sm mb-2']"), 3)
+
+		WebUI.click(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//button[@data-testid='cart-btn-summary-cta']"))
+
 		WebUI.closeBrowser()
 	}
-	
+
 	@And("user pilih metode pembayaran")
 	def userPilihPaymentMethod() {
-		
 	}
 
 	@Then("user berhasil melakukan pembayaran")
 	def userVerifySuccessPayment() {
-		
 	}
 }
